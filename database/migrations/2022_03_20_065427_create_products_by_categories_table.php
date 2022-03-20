a719a09553ef17543c8c8e23cdd85430c7ca8c6a
G'anisher <git@ganisher.kz>
2022-03-20 12:57:34 +0600

    catalog - migration to products by categories relation

---
+++ database/migrations/2022_03_20_065427_create_products_by_categories_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('products_by_categories', function (Blueprint $table) {
+            $table->foreignId('product_id')->references('id')->on('products')->cascadeOnDelete();
+            $table->foreignId('category_id')->references('id')->on('categories')->cascadeOnDelete();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('products_by_categories');
+    }
+};
