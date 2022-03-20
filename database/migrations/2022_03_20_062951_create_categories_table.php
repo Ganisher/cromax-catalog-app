8b16c36e40178d1769dc56dc389d4424e037c059
G'anisher <git@ganisher.kz>
2022-03-20 12:43:12 +0600

    catalog - migration to categories table

---
+++ database/migrations/2022_03_20_062951_create_categories_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('categories', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('parent_id')->references('id')->on('categories');
+            $table->string('sku')->unique();
+            $table->string('title');
+            $table->string('slug');
+            $table->tinyInteger('sort_order')->unsigned()->default(0);
+            $table->tinyInteger('status')->unsigned()->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('categories');
+    }
+};
